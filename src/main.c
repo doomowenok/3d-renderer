284320a5830605ca3a802f3684457f54fc50a371
doomowenok <doomowenok@icloud.com>
2025-01-09 17:29:08 +0300

    Creating window.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+
+    window = SDL_CreateWindow(
+        "3D Renderer",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+    );
+
+    if (!window)
+    {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        0
+    );
+
+    if (!renderer)
+    {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
+    }
+
+    return true;
+}
+
+int main(void)
+{
+    is_running = initialize_window();
+
     return 0;
 }
