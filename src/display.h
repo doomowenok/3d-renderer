08a61701c4fda9e81741becb11027e5b06453c3a
doomowenok <doomowenok@icloud.com>
2025-01-10 16:02:58 +0300

    Refactoring.

---
+++ src/display.h
@@ -0,0 +1,27 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <SDL.h>
+
+extern SDL_Window* window;
+extern SDL_Renderer* renderer;
+
+extern uint32_t* color_buffer;
+extern SDL_Texture* color_buffer_texture;
+
+extern int window_width;
+extern int window_height;
+
+bool initialize_window(void);
+
+void draw_grid(const int per_segment, const uint32_t color);
+void draw_rect(const int x, const int y, const int width, const  int height,  const uint32_t color);
+
+void render_color_buffer(void);
+void clear_color_buffer(uint32_t color);
+
+void destroy_window(void);
+
+#endif
